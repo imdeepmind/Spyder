16be65f0bdade7dce4016310e095564dc7385354
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-08-02 01:29:09 +0530

    added code to clean the words

--- parse/parse.py
+++ parse/parse.py
@@ -30,12 +30,47 @@ class Parser:
     
     return list(set(page_links))
 
+  def __clean_text(self, text):
+    text = text.lower()
+
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"aren't", "are not", text)
+    text = re.sub(r"couldn't", "could not", text)
+    text = re.sub(r"didn't", "did not", text)
+    text = re.sub(r"doesn't", "does not", text)
+    text = re.sub(r"don't", "do not", text)
+    text = re.sub(r"hadn't", "had not", text)
+    text = re.sub(r"hasn't", "has not", text)
+    text = re.sub(r"haven't", "have not", text)
+    text = re.sub(r"isn't", "is not", text)
+    text = re.sub(r"hadn't", "had not", text)
+    text = re.sub(r"hadn't", "had not", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"mightn't", "might not", text)
+    text = re.sub(r"mustn't", "must not", text)
+    text = re.sub(r"needn't", "need not", text)
+    text = re.sub(r"shouldn't", "should not", text)
+    text = re.sub(r"wasn't", "was not", text)
+    text = re.sub(r"weren't", "were not", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"wouldn't", "would not", text)
+
+    text = re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', '', text, flags=re.MULTILINE)
+    text = re.sub(r'(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)', '', text)
+    text = re.sub(r'\b[0-9]+\b\s*', '', text)
+
+    return text
+
   def __get_keywords(self):
     page_text = self.__soup.get_text()
 
-    # word tokenizing
-    words = page_text.split(' ')
+    # clean the text
+    page_text = self.__clean_text(page_text)
 
+    # word tokenizing
+    words = [word for word in page_text.split(' ') if word]
+    
     # removing stop words
     words_without_sw= [word for word in words if not word in all_stopwords]
 
